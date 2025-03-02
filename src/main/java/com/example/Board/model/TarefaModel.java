8752c6d18dc1f2d97226b3e6f7fe36202d06f544
devlucascalado <devlucascalado@gmail.com>
2025-03-02 19:50:17 -0300

    terminando board

--- src/main/java/com/example/Board/model/TarefaModel.java
+++ src/main/java/com/example/Board/model/TarefaModel.java
@@ -2,27 +2,26 @@ package com.example.Board.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
+
 @Data
 @Entity
 @Table(name = "tb_tarefas")
-public class TarefasModel {
+public class TarefaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 100)
     private String titulo;
 
-    @Column(length = 1200, nullable = false)
+    @Column(length = 1200)
     private String descricao;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 20)
     private String status;
 
     @Column(name = "data_criacao")
